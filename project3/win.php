585b00e921ef8f0bbda7c93019cfdd983d347d32
liamtw22 <31185529+liamtw22@users.noreply.github.com>
2019-12-10 09:47:33 -0500

    Create win.php

---
+++ project3/win.php
@@ -0,0 +1,36 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'newpassword';
+$DATABASE_NAME = 'phplogin';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit();
+}
+$stmt = $con->prepare('SELECT wins,loss,tie FROM accounts WHERE id = ?');
+// In this case we can use the account ID to get the account info.
+$stmt->bind_param('i', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($wins, $loss,$tie);
+$stmt->fetch();
+$stmt->close();
+$wins++;
+$getid=$_SESSION['id'];
+$sql = "UPDATE accounts SET wins=$wins WHERE id=$getid";
+if ($con->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $con->error;
+}
+$con->close();
+header('Location: check.php');
+?>
